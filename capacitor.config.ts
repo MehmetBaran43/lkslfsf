0a21556c55dd1afeac440e7b4e0adb9d35f08d2c
MehmetBaran43 <ilhankacak@gmail.com>
2026-04-27 19:58:15 +0300

    Githuba hazır hale getirdim

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.huzurvakit.app',
+  appName: 'Huzur Vakit',
+  webDir: 'dist'
+};
+
+export default config;
